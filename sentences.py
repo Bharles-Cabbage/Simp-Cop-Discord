379fb11ccee74d88f81b2ada5fd68657dc6c7b56
konbo <konbo666@tutanota.com>
2021-02-14 22:28:28 +0530

    Added simping like phrases

---
+++ sentences.py
@@ -0,0 +1,2 @@
+# Simp phrases
+phrases = ["die in your thighs", "nice feet", "hot feet", "feet pics", "nice rack", "nice boobs", "nice ass", "nice assets", "milkers", "milk truck", "big boobs", "big ass","spank worthy ass","perky breasts"]
