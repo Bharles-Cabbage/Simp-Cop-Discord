6ab08dc22bb0a01a3dc072051a19cf5a8f792913
konbo <konbo666@tutanota.com>
2021-02-14 22:56:49 +0530

    Phrases added

--- sentences.py
+++ sentences.py
@@ -1,2 +1,19 @@
 # Simp phrases
-phrases = ["die in your thighs", "nice feet", "hot feet", "feet pics", "nice rack", "nice boobs", "nice ass", "nice assets", "milkers", "milk truck", "big boobs", "big ass","spank worthy ass","perky breasts"]
+phrases = [
+    "die in your thighs",
+    "nice feet",
+    "hot feet",
+    "feet pics",
+    "nice rack", 
+    "nice boobs",
+    "nice ass", 
+    "nice assets", 
+    "milkers", 
+    "milk truck", 
+    "big boobs", 
+    "big ass",
+    "spank worthy ass",
+    "perky breasts",
+    "hot nipples",
+    "perky nipples",
+]
